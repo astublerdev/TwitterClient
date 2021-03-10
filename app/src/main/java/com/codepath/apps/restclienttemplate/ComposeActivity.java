715167cb85704b8247a1e7e13a98da76bef42410
annastublerdev <annastubler@gmail.com>
2021-03-10 15:23:24 -0800

    Part 3 of Twitter #2 started

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -40,6 +40,7 @@ public class ComposeActivity extends AppCompatActivity {
                 }
                 Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
                 // Make an API call to Twitter to publish the tweet
+
             }
         });
     }
